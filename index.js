6e31086300594c5b8ce713eb7fe9ba938f5acc6b
Raveen K Sharma <raveen08@gmail.com>
2021-02-08 23:04:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // Write your code here
 
 class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+class Lunch {
   constructor(name, breed) {
     this.name = name;
     this.breed = breed;
